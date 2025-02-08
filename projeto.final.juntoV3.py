7afdc1a704a40a603d270d840668163236e295f7
Laura-C-M <shark01963@sharkcoders.pt>
2025-02-08 09:37:34 +0000

    projeto.final.juntoV3.py

--- projeto.final.juntoV3.py
+++ projeto.final.juntoV3.py
@@ -126,8 +126,8 @@ def bau(jogador):
             ouro_ganho = random.randint(80, 100)
 
         print(f"\nEncontraste um baú de tesouro! Ele contém {ouro_ganho} ouro!")
-        return ouro_ganho
         jogador["ouro"] += ouro_ganho
+        return ouro_ganho
 
 def boss_final(jogador):
     boss = Enemy("Dragon Shark", 120, 20, 100, 100)
